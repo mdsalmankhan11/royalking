f1f934dcc0874b4103f8949fad0aa5e7433ef23b
mdsalmankhan11 <85951638+mdsalmankhan11@users.noreply.github.com>
2021-08-25 00:30:17 +0530

    royalking.py
    https://github.com/mdsalmankhan11/royalking.git
    ls
    cd royalking
    Python2 royalking.py

---
+++ royalking.py
@@ -0,0 +1,255 @@
+#!/usr/bin/python2
+
+#coding=utf-8
+
+#MD SALMAN KHAN
+
+#programer KABIR SINGH
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib
+
+def jalan(z):
+
+	for e in z + '\n':		sys.stdout.write(e)
+
+		sys.stdout.flush()
+
+		time.sleep(00.01)
+
+#### colours ####
+
+B='\033[1;94m'
+
+R='\033[1;91m'
+
+G='\033[1;92m'
+
+W='\033[1;97m'
+
+S='\033[1;96m'
+
+P='\033[1;95m'
+
+Y='\033[1;93m'
+
+os.system("clear")
+
+jalan('\033[1;95m------------------------------------------------')
+
+jalan('\033[1;93m   _     _     _     _     _     _     _     _  ')
+
+jalan('\033[1;94m  / \   / \   / \   / \   / \   / \   / \   / \ ')
+
+jalan('\033[1;95m ( B ) ( I ) ( R ) ( T ) ( H ) ( D ) ( A ) ( Y )')
+
+jalan('\033[1;92m  \_/   \_/   \_/   \_/   \_/   \_/   \_/   \_/ ')
+
+jalan('\033[1;94m------------------------------------------------')
+
+jalan('\033[1;93m HAPPY BIRTHDAY  FAIZAN KHAN====>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY FAIZAN KHAN=====>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN======>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN=======>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY FAIZAN KHAN========>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN=========>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY FAIZAN KHAN==========>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN===========>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY FAIZAN KHAN============>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN=============>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY FAIZAN KHAN==============>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN===============>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN===============>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY FAIZAN KHAN================>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN=================>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN==================>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY FAIZAN KHAN===================>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN====================>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN=====================>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN======================>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY FAIZAN KHAN======================>')
+
+jalan('\033[1;95m------------------------------------------------')
+
+jalan('\033[1;93m   _     _     _     _     _     _     _     _  ')
+
+jalan('\033[1;94m  / \   / \   / \   / \   / \   / \   / \   / \ ')
+
+jalan('\033[1;95m ( B ) ( I ) ( R ) ( T ) ( H ) ( D ) ( A ) ( Y )')
+
+jalan('\033[1;92m  \_/   \_/   \_/   \_/   \_/   \_/   \_/   \_/ ')
+
+jalan('\033[1;94m------------------------------------------------')
+
+###TECH BABA###
+
+## PROGRAMMER ##
+
+os.system("clear")
+
+print """\033[1;90m"""
+
+jalan('\033[1;94m------------------------------------------------')
+
+jalan('\033[1;94m------------------------------------------------')
+
+jalan('\033[1;92m _    _              _____    _____   __     __')
+
+jalan('\033[1;93m| |  | |     /\     |  __ \  |  __ \  \ \   / /')
+
+jalan('\033[1;93m| |__| |    /  \    | |__) | | |__) |  \ \_/ / ')
+
+jalan('\033[1;92m|  __  |   / /\ \   |  ___/  |  ___/    \   /  ')
+
+jalan('\033[1;93m| |  | |  / ____ \  | |      | |         | |   ')
+
+jalan('\033[1;92m|_|  |_| /_/    \_\ |_|      |_|         |_|   ')
+
+jalan('\033[1;95m------------------------------------------------')
+
+jalan('\033[1;93m   _     _     _     _     _     _     _     _  ')
+
+jalan('\033[1;94m  / \   / \   / \   / \   / \   / \   / \   / \ ')
+
+jalan('\033[1;95m ( B ) ( I ) ( R ) ( T ) ( H ) ( D ) ( A ) ( Y )')
+
+jalan('\033[1;92m  \_/   \_/   \_/   \_/   \_/   \_/   \_/   \_/ ')
+
+jalan('\033[1;94m------------------------------------------------')
+
+jalan('\033[1;93m   HAPPY BIRTHDAY FAIZAN
+
+jalan('\033[1;93m  HAPPY BIRTHDAY  FAIZAN
+
+jalan('\033[1;93m   HAPPY BIRTHDAY  FAIZAN
+
+jalan('\033[1;92m  HAPPY 
+
+jalan('\033[1;92m                      BIRTHDAY
+
+jalan('\033[1;93m                                                  THE ROYAL KING KHAN
+
+print""
+
+jalan('\033[1;91m------------------------------------------------')
+
+jalan('\033[1;92m   [->  ROGRAMMER KABIR SINGH <-]')
+
+jalan('\033[1;94m------------------------------------------------')
+
+jalan('\033[1;92m------------------------------------------------')
+
+jalan('\033[1;94m>BIRTHDAY WISH BY SALMAN ARSHALAN KAMRAN   <')
+
+jalan('\033[1;92m------------------------------------------------')
+
+jalan('\033[1;93m      > THE ROYAL KING KHAN <')
+
+jalan('\033[1;94m------------------------------------------------')
+
+birthday= "KABIR SINGH"
+
+loop = 'true'
+
+while (loop == 'true'):
+
+    print""#KABIR SINGH  
+
+    print""
+
+    happy = raw_input("\033[1;96m<<-->> \x1b[0;36m TYPE GO AND ENTER \x1b[1;92m>>> ")
+
+    if (happy == birthday):
+
+            loop = 'false'
+
+    else:
+
+        print "Loading..."
+
+        os.system('xdg-open https://www.facebook.com/profile.php?id=100056902457494
+
+jalan('\033[1;95m------------------------------------------------')
+
+jalan('\033[1;93m   _     _     _     _     _     _     _     _  ')
+
+jalan('\033[1;94m  / \   / \   / \   / \   / \   / \   / \   / \ ')
+
+jalan('\033[1;95m ( B ) ( I ) ( R ) ( T ) ( H ) ( D ) ( A ) ( Y )')
+
+jalan('\033[1;92m  \_/   \_/   \_/   \_/   \_/   \_/   \_/   \_/ ')
+
+jalan('\033[1;94m------------------------------------------------')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN====>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY THE ROYAL KINGH KHAN=====>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN======>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN=======>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY THE ROYAL KINGH KHAN========>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN=========>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY THE ROYAL KINGH KHAN==========>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN===========>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY THE ROYAL KINGH KHAN============>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN=============>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY THE ROYAL KINGH KHAN==============>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN===============>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY THE ROYAL KINGH KHAN================>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN=================>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN==================>')
+
+jalan('\033[1;92m HAPPY BIRTHDAY THE ROYAL KINGH KHAN===================>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN====================>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN=====================>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN======================>')
+
+jalan('\033[1;93m HAPPY BIRTHDAY THE ROYAL KINGH KHAN======================>')
+
+jalan('\033[1;95m------------------------------------------------')
+
+jalan('\033[1;93m   _     _     _     _     _     _     _     _  ')
+
+jalan('\033[1;94m  / \   / \   / \   / \   / \   / \   / \   / \ ')
+
+jalan('\033[1;95m ( B ) ( I ) ( R ) ( T ) ( H ) ( D ) ( A ) ( Y )')
+
+jalan('\033[1;92m  \_/   \_/   \_/   \_/   \_/   \_/   \_/   \_/ ')
+
+jalan('\033[1;94m------------------------------------------------')
+
+##### HATTER STAY AWAY #####
